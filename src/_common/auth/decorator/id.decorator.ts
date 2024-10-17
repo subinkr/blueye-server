f0ee6112ee2b74ae3040d43b15e76884403a926a
subin <github@subin.kr>
2024-10-17 17:40:46 +0900

    EDIT: 241017 base code

--- src/_common/auth/decorator/id.decorator.ts
+++ src/_common/auth/decorator/id.decorator.ts
@@ -8,9 +8,9 @@ export const AuthId = createParamDecorator((_, context: ExecutionContext) => {
   const req = context.switchToHttp().getRequest();
   const id = req.id;
 
-  if (id === undefined || id === null) {
-    throw new UnauthorizedException('로그인이 필요합니다.');
-  }
+  // if (id === undefined || id === null) {
+  //   throw new UnauthorizedException('로그인이 필요합니다.');
+  // }
 
   return id;
 });
